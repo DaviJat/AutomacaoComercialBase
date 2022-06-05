1ae8a73965467f22be4c153cf01d778994860aad
DaviJat <ddavijatoba33@gmail.com>
2022-06-05 15:53:09 -0300

    Comentários JavaDoc da Abstract View

--- src/view/AbstractView.java
+++ src/view/AbstractView.java
@@ -4,14 +4,34 @@ import java.util.ArrayList;
 
 public abstract class AbstractView {
 	
+	/**
+	 * View com o Menu Principal do Gerenciamento Selecionado
+	 */
 	public abstract void menuPrincipal();
 	
+	/**
+	 * View com o menu de Cadastro, retornando uma Lista com os dados inseridos pelo Usuário	
+	 * @return
+	 */
 	public abstract ArrayList<String> menuCadastro();
 	
+	/**
+	 * Tela com o menu de Edição, retornando uma lista com a referência do campo a ser editado
+	 * e o novo valor que irá substitui-lo 
+	 * @return
+	 */
 	public abstract ArrayList<String> menuEdicao();
 	
+	/**
+	 * View com o menu de Exclusão, retornando código do elemento a ser excluído
+	 * @return
+	 */
 	public abstract String menuExcluir();
 	
+	/**
+	 * View para listagem dos elementos cadastrados
+	 * @return
+	 */
 	public abstract void listar();
 
 }
